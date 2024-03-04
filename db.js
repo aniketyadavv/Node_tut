a015247033c8387c5b885e79e08fca11d918dbe7
Aniket Yadav <aniket912yadav@gmail.com>
2024-03-04 15:10:42 +0530

    Database add into server.js

--- db.js
+++ db.js
@@ -1,3 +1,28 @@
 const mongoose = require('mongoose');
 
-// define the mongoose connection
+// define the mongoose connection URL
+const mongoURL = 'mongodb://localhost:27017/hotels'
+
+// setup MongoDB connection
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+// get default connection
+// mongoose maintain a default connection object representing the MongoDB connection
+const db = mongoose.connection;
+
+//define event listeners, when event occur it will give messages
+db.on('connected', () => {
+    console.log("connected to MOngoDB Server");
+});
+db.on('error', (err) => {
+    console.log("error in MongoDB Server", err);
+});
+db.on('disconnected', () => {
+    console.log("Disconnected to MOngoDB Server");
+});
+
+//export the database connection
+module.exports = db;
